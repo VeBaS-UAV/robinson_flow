bdae820ddf171642584cca192029a7ce8d6ef59f
Matthias Füller <matthias.fueller@fhdw.de>
2022-01-05 09:33:08 +0100

    init commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+# MamoGe Ryven
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from distutils.extension import Extension
+
+setuptools.setup(
+    name="mamoge-ryven",
+    version="0.0.1",
+    author="matthias fueller",
+    author_email="matthias.fueller@fhdw.de",
+    description="mamoge task planner",
+    long_description="TBD",
+    long_description_content_type="text/markdown",
+    url="https://github.com/TBD",
+    project_urls={
+        "Bug Tracker": "https://github.com/TBD",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9"
+)
