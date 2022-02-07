5dfc3288e9962df6790ef8002eb632de6b2c44ff
Matthias Füller <matthias.fueller@fhdw.de>
2022-02-07 13:44:29 +0100

    put package build stuff in own package

--- robinson_flow/pyflow_nodes/Robinson/__init__.py
+++ robinson_flow/pyflow_nodes/Robinson/__init__.py
@@ -17,6 +17,7 @@ from robinson_flow.pyflow_nodes.Robinson.Nodes.utils import EvalNode, LambdaNode
 from robinson_flow.pyflow_nodes.Robinson.Pins.MavlinkPin import MavlinkPin
 from robinson_flow.pyflow_nodes.Robinson.Tools.ConfigDockTool import ConfigDockTool
 
+from robinson_flow.pyflow_nodes.Robinson.package_utils import export_nodes
 # # Function based nodes
 # # from robinson_flow.pyflow_nodes.Robinson.FunctionLibraries.DemoLib import DemoLib
 
@@ -34,8 +35,6 @@ from robinson_flow.pyflow_nodes.Robinson.Tools.ConfigDockTool import ConfigDockT
 # # Prefs widgets
 # from robinson_flow.ryven_nodes.nodes.components import PrintOutputComponent, TestComponent
 
-# from robinson_flow.pyflow_nodes.Robinson.Nodes.ExternalNodes import ExternalSink, ExternalSource
-# from robinson_flow.pyflow_nodes.Robinson.Nodes.BaseNode import AddHelloComponent, OutputNameComponent, RobinsonPyFlowBase, RobinsonPyFlowFunc, RobinsonTicker
 
 _FOO_LIBS = {}
 _NODES = {}
@@ -55,116 +54,6 @@ _EXPORTERS[RobinsonExporter.__name__] = RobinsonExporter
 
 # _PREFS_WIDGETS["Demo"] = DemoPrefs
 
-# import inspect
-# import sys
-
-
-def factory(cls):
-    name = cls.__name__
-    if name.endswith("Component"):
-        name = name[:-len("Component")]
-
-    name = name.strip("_/-")
-    # print(f"generate component {name}:{cls}")
-    class PyflowTemplateNode(RobinsonPyFlowBase):
-        def __init__(self, name, uid=None):
-            super().__init__(name, cls=cls, uid=uid)
-
-        @staticmethod
-        def category():
-            return cls.__module__.replace(".","|")
-
-    cl =  PyflowTemplateNode
-    cl.__name__ = name
-
-    return name, cl
-
-def factory_function(cb):
-    name = cb.__name__
-
-    # print(f"generate component {name}:{cls}")
-    class RobinsonPyFlowFunctionWrapperFactory(RobinsonPyFlowFunc):
-        def __init__(self, name, uid=None):
-            super().__init__(name, cb=cb, uid=uid)
-
-        @staticmethod
-        def category():
-            return cb.__module__.replace(".","|")
-
-
-    cl =  RobinsonPyFlowFunctionWrapperFactory
-    cl.__name__ = name
-
-    return name, cl
-
-
-def load_components_from_module(module):
-    import inspect
-    import sys
-    results =  []
-    for name, obj in inspect.getmembers(module):
-        if inspect.isclass(obj):
-            if sys.modules[obj.__module__] == module:
-                if issubclass(obj, Component):
-                    results.append(obj)
-    return results
-
-def export_nodes():
-    import importlib
-    from robinson_flow.config import settings
-
-    robinson_packages = []
-    component_list = []
-    function_list = []
-
-    try:
-        robinson_packages = settings.robinson.modules
-
-
-        for rob_pkg in robinson_packages:
-            try:
-                module = importlib.import_module(rob_pkg)
-                component_list.extend(load_components_from_module(module))
-            except Exception as e:
-                print("Could not load module")
-                print(e)
-
-    except Exception as e:
-        print("Could not load robinson components from config")
-        print(e)
-
-
-    rob_comps = {k:v for k,v in [factory(c) for c in component_list]}
-
-    function_names = []
-    try:
-        function_names = settings.robinson.functions #.append("")
-
-        for rob_pkg in function_names:
-            module = ".".join(rob_pkg.split(".")[:-1])
-            name = rob_pkg.split(".")[-1]
-            module = importlib.import_module(module)
-            func = getattr(module, name)
-            function_list.append(func)
-    except Exception as e:
-        print("Could not load function from module")
-        print(e)
-
-    func_comps = {k:v for k,v in [factory_function(f) for f in function_list]}
-
-    other_comp = {}
-
-    other_comp["ExternalSource"] = ExternalSource
-    other_comp["ExternalSink"] = ExternalSink
-    other_comp["FrameView"] = FrameView
-    other_comp["LambdaNode"] = LambdaNode
-    other_comp["LoggingView"] = LoggingView
-    other_comp["RobinsonTicker"] = RobinsonTicker
-    other_comp["OnMessageExec"] = OnMessageExec
-    other_comp["EvalNode"] = EvalNode
-    other_comp["PlotView"] = PlotView
-
-    return {**rob_comps, **func_comps, **other_comp}
 
 class Robinson(IPackage):
 	def __init__(self):
---
+++ robinson_flow/pyflow_nodes/Robinson/package_utils.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+from robinson.components import Component
+from robinson_flow.pyflow_nodes.Robinson.Nodes.BaseNode import RobinsonPyFlowBase, RobinsonPyFlowFunc, RobinsonTicker
+from robinson_flow.pyflow_nodes.Robinson.Nodes.ExternalNodes import ExternalSink, ExternalSource
+from robinson_flow.pyflow_nodes.Robinson.Nodes.OpenCV import FrameView
+from robinson_flow.pyflow_nodes.Robinson.Nodes.utils import EvalNode, LambdaNode, LoggingView, OnMessageExec, PlotView
+
+
+def factory(cls):
+    name = cls.__name__
+    if name.endswith("Component"):
+        name = name[:-len("Component")]
+
+    name = name.strip("_/-")
+    # print(f"generate component {name}:{cls}")
+    class PyflowTemplateNode(RobinsonPyFlowBase):
+        def __init__(self, name, uid=None):
+            super().__init__(name, cls=cls, uid=uid)
+
+        @staticmethod
+        def category():
+            return cls.__module__.replace(".","|")
+
+    cl =  PyflowTemplateNode
+    cl.__name__ = name
+
+    return name, cl
+
+def factory_function(cb):
+    name = cb.__name__
+
+    # print(f"generate component {name}:{cls}")
+    class RobinsonPyFlowFunctionWrapperFactory(RobinsonPyFlowFunc):
+        def __init__(self, name, uid=None):
+            super().__init__(name, cb=cb, uid=uid)
+
+        @staticmethod
+        def category():
+            return cb.__module__.replace(".","|")
+
+
+    cl =  RobinsonPyFlowFunctionWrapperFactory
+    cl.__name__ = name
+
+    return name, cl
+
+
+def load_components_from_module(module):
+    import inspect
+    import sys
+    results =  []
+    for name, obj in inspect.getmembers(module):
+        if inspect.isclass(obj):
+            if sys.modules[obj.__module__] == module:
+                if issubclass(obj, Component):
+                    results.append(obj)
+    return results
+
+def export_nodes():
+    import importlib
+    from robinson_flow.config import settings
+
+    robinson_packages = []
+    component_list = []
+    function_list = []
+
+    try:
+        robinson_packages = settings.robinson.modules
+
+
+        for rob_pkg in robinson_packages:
+            try:
+                module = importlib.import_module(rob_pkg)
+                component_list.extend(load_components_from_module(module))
+            except Exception as e:
+                print("Could not load module")
+                print(e)
+
+    except Exception as e:
+        print("Could not load robinson components from config")
+        print(e)
+
+
+    rob_comps = {k:v for k,v in [factory(c) for c in component_list]}
+
+    function_names = []
+    try:
+        function_names = settings.robinson.functions #.append("")
+
+        for rob_pkg in function_names:
+            module = ".".join(rob_pkg.split(".")[:-1])
+            name = rob_pkg.split(".")[-1]
+            module = importlib.import_module(module)
+            func = getattr(module, name)
+            function_list.append(func)
+    except Exception as e:
+        print("Could not load function from module")
+        print(e)
+
+    func_comps = {k:v for k,v in [factory_function(f) for f in function_list]}
+
+    other_comp = {}
+
+    other_comp["ExternalSource"] = ExternalSource
+    other_comp["ExternalSink"] = ExternalSink
+    other_comp["FrameView"] = FrameView
+    other_comp["LambdaNode"] = LambdaNode
+    other_comp["LoggingView"] = LoggingView
+    other_comp["RobinsonTicker"] = RobinsonTicker
+    other_comp["OnMessageExec"] = OnMessageExec
+    other_comp["EvalNode"] = EvalNode
+    other_comp["PlotView"] = PlotView
+
+    return {**rob_comps, **func_comps, **other_comp}
