64e55e7305a3ba92f9d472723380c780deff7423
Matthias Füller <matthias.fueller@fhdw.de>
2022-04-13 09:06:43 +0200

    switched to yaml config

--- robinson_flow/config.py
+++ robinson_flow/config.py
@@ -3,12 +3,11 @@
 from dynaconf import Dynaconf
 
 def _get_config_files():
-    return ['settings.toml']
+    return ['settings.yaml']
 
 settings = Dynaconf(
     environments=True,
     envvar_prefix="ROBINSON_FLOW",
     env_switcher="ROBINSON_FLOW_MODE",
-    # root_path="/home/matthias/src/robinson/robinson_flow/robinson_flow",
     settings_files=_get_config_files(),
 )
